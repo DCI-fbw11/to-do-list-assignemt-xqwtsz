4e45920d948f5e060ae93a2a4bb83fa377ebf6e9
xqwtsz <xqwtsz@gmail.com>
2018-11-08 16:04:04 +0100

    trying to delete things

--- main.js
+++ main.js
@@ -5,14 +5,22 @@ class List {
   }
 
   add(item) {
+    let newItem = document.createElement("li");
+    newItem.innerHTML +=
+      `<input type="checkbox" onclick="check();" value="Select" />` +
+      " " +
+      item +
+      " " +
+      `<a class="deleteButton" onclick="deleteItem();"tag="pepperoni" >Delete</a>`;
     if (this.list.length == 0) {
-      this.list.push(item);
+      this.list.push(newItem);
     } else {
-      this.list.unshift(item);
+      this.list.unshift(newItem);
     }
     //this.list.push(item);
     this.render();
   }
+
   cleanList() {
     while (this.name.firstChild) {
       this.name.removeChild(this.name.firstChild);
@@ -21,37 +29,25 @@ class List {
   render() {
     this.cleanList();
     for (let member = 0; member < this.list.length; member++) {
-      let newItem = document.createElement("li");
-      newItem.innerHTML +=
-        `<input type="checkbox" onclick="List.prototype.check();" tag="${member}" value="Select" />` +
-        " " +
-        this.list[member] +
-        " " +
-        `<a class="deleteButton" onclick="List.prototype.link();" tag="${member}">Delete</a>`;
-      this.name.appendChild(newItem);
+      this.list[member].tag = member;
+      this.name.appendChild(this.list[member]);
     }
   }
 
-  delete(item) {}
-
+  deleteItem() {
+    console.log("link clicked!");
+    for (var i = 0; i < this.list.length; i++) {
+      this.list[i].addEventListener("click", function() {
+        this.parentNode.removeChild(this);
+      });
+    }
+  }
   check() {
     console.log("checkbox clicked!");
-    var current = this;
-    for (var i = 0; i < elem.length; i++) {
-      if (current != elem[i]) {
-        elem[i].classList.remove("active");
-      } else if (current.classList.contains("active") === true) {
-        current.classList.remove("active");
-      } else {
-        current.classList.add("active");
-      }
-    }
-  }
-
-  link() {
-    console.log("link clicked!");
     console.log(this);
   }
+
+  link() {}
 }
 
 const theForm = document.getElementById("toDoForm");
@@ -66,3 +62,17 @@ theForm.addEventListener("submit", e => {
 });
 
 console.log(javascriptList.list);
+
+function check() {
+  javascriptList.check();
+}
+
+function deleteItem() {
+  javascriptList.deleteItem();
+  // var listItems = document.getElementsByTagName("li"); // or document.querySelectorAll("li");
+  // for (var i = 0; i < listItems.length; i++) {
+  //   listItems[i].addEventListener("click", function() {
+  //     this.parentNode.removeChild(this);
+  //   });
+  // }
+}
